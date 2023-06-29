b1f3a6225f667e061f5557f687710f33442e8db9
steffendreesen <sdreese@clemson.edu>
2023-06-29 09:20:46 -0400

    fix drawsvg add

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,8 +1,6 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-m.png'
-import { useEffect, useRef } from 'react'
-import { DrawSVGPlugin } from 'gsap-trial/DrawSVGPlugin'
-import gsap from 'gsap-trial'
+import { useRef } from 'react'
 
 const Logo = () => {
 
@@ -10,33 +8,6 @@ const Logo = () => {
     const outlineLogoRef = useRef()
     const solidLogoRef = useRef()
 
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
-    
-        gsap.timeline()
-          .to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-          })
-          .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-          })
-    
-        gsap.fromTo(
-          solidLogoRef.current,
-          {
-            opacity: 0,
-          },
-          {
-            opacity: 1,
-            delay: 4,
-            duration: 4,
-          }
-        )
-      }, [])
-
-    
     return (
         <div className="logo-container" ref={bgRef}>
             <img ref={solidLogoRef} className="solid-logo" src={LogoS} alt="M" />
